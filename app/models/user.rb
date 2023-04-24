24f328aabefa25f89927092a06b08e1979b5aab3
CHIMEZIE ASOUZU <shegy28@gmail.com>
2023-04-24 15:20:26 +0200

    Add validation to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_many :expenses, dependent: :destroy
     has_many :groups, dependent: :destroy
+
+    validates :name, presence: true, length: { in: 3..50 }
+
 end
